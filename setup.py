9b5176f68c230fef82cdf044648bb3cb22e85fac
Paolo Barazon <titopao@gmail.com>
2012-07-16 10:42:34 +0800

    added params to setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Terry',
+	version='0.1',
+	description='Because there are two Terrys in the Monty Python troupe ;-)',
+	author='Allan Paolo Barazon',
+	author_email='titopao@gmail.com',
+	url='https://github.com/titopao/terry',
+	py_modules=['terry'],
+	)
